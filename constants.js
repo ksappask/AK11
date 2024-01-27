544f1e969ee04b79d2ace66f3b2f4283fb722b84
Akash Krishnan <33875953+ksappask@users.noreply.github.com>
2024-01-27 23:11:48 +0530

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,5 +1,1 @@
-export const CRICKET_MATCH_LIST_API =
-  "https://us-central1-elevengame.cloudfunctions.net/app/v1/cricket/allMatches";
-export const CAROUSEL_IMAGE_LIST_API =
-  "https://us-central1-elevengame.cloudfunctions.net/app/v1/allCarouselImages";
-export const AUTH_API_KEY = "AIzaSyATia0DrKNNjMrZL3GV_eR4XFUQP6_sMiQ";
+
